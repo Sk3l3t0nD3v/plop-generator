53f52415e7bcef429df677561d08dcf7551eae3a
Skeleton <Skeleton@gmail.com>
2021-09-28 13:55:12 +0200

    add modify file for add module imported

--- actions/npm.js
+++ actions/npm.js
@@ -1,8 +1,33 @@
-module.exports = ({ packages }) => {
-  return [
-    {
-      type: 'npmInstallPackages',
-      install: packages,
-    },
-  ];
+const path = require('path');
+module.exports = ({ package, module, where }) => {
+  const actions = [];
+  const rootAppPath = path.resolve(__dirname + '/../src/app');
+  console.log(`${rootAppPath}/${where}`);
+  actions.push({
+    type: 'npmInstallPackages',
+    install: package,
+  });
+
+  //insert module inported to module of app
+  actions.push({
+    type: 'modify',
+    path: `${rootAppPath}/${where}`,
+    pattern: /;/m,
+    template: ";\r\nimport { {{module}} } from '{{package}}';",
+  });
+  actions.push({
+    type: 'modify',
+    path: `${rootAppPath}/${where}`,
+    pattern: /imports: \[/g,
+    template: `imports: [ \r\n ${module},`,
+  });
+  return actions;
 };
+
+/* insert text between
+<img src="/assets/img/image.jpg">
+str.replace(/(=['"])(\/?assets\/img)/g, '$1mypath$2')
+              ^^^^^  ^^^^^^^^^^^^^^      ^^      ^^-
+              1st     2nd               1st       2nd
+result
+<img src="mypath/assets/img/image.jpg">*/
--- prompts/npm.js
+++ prompts/npm.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    name: 'packages',
+    name: 'package',
     type: 'checkbox',
     choices: [
       { name: '[Angular] Add Formly', value: '@ngx-formly/core' },
@@ -13,5 +13,38 @@ module.exports = [
       { name: '[Angular] Add Strars Rating', value: 'ng-starrating' },
       { name: '[Ionic] Add Storage Angular', value: '@ionic/storage-angular' },
     ],
+    validate: (input) => {
+      if (/.+/.test(input)) {
+        return true;
+      }
+      return 'Scegli uno o più pacchetti';
+    },
+  },
+  {
+    when: (context) => angularPackageFound(context.package[0]),
+    name: 'module',
+    message: 'Inserisci il nome del modulo da importare :',
+    type: 'input',
+    validate: (input) => {
+      if (/.+/.test(input)) {
+        return true;
+      }
+      return 'Nome modulo is required';
+    },
+  },
+  {
+    when: (context) => angularPackageFound(context.package[0]),
+    name: 'where',
+    message: 'Dove vuoi importare il modulo ex:profile/profile.module.ts?',
+    type: 'input',
+    default: 'app.module.ts',
   },
 ];
+
+const angularPackageFound = (package) => {
+  if (!package) return false;
+  const modules = ['angular', 'ionic', '@ngx', 'ng-'];
+  const found =
+    modules.filter((m) => package.includes(m)).length > 0 ? true : false;
+  return found;
+};
